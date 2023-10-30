59d14eb1903184a9905adb83599b50a61f6e6a65
lathiesh24 <lathumahi24@gmail.com>
2023-10-31 03:24:06 +0530

    recent

--- models/queryModel.js
+++ models/queryModel.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
+const answerSchema = new mongoose.Schema({
+  questionNumber: String,
+  sqlAnswer: String,
+});
+
 const querySchema = new mongoose.Schema({
   teamName: String,
-  answers: [
-    {
-      questionNumber: String,
-      sqlAnswer: String,
-    },
-  ],
+  answers: [answerSchema],
 });
 
 const Query = mongoose.model('Query', querySchema);
